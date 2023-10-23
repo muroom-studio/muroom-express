5ca724622f908c5deaa8e5a3fe6daa146240802b
taehwan01 <monte6198@gmail.com>
2023-10-23 20:09:11 +0900

    Initialize muroom-express

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express(); // express 서버 생성
+
+app.get('/', (req, res) => {
+  res.status(200).send('[muroom studio] Hello, world.');
+});
+
+// 서버 시작
+const port = process.env.PORT || 3000;
+const url = process.env.URL || `http://localhost:${port}`;
+const server = app.listen(port, () => {
+  console.log(`[muroom studio] Express server listening on port ${port}\n[muroom studio] API: ${url}`);
+});
