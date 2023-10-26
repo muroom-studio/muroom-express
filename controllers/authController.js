9d00fb2d646d4b26e0b069db95416f01419ab7dc
Monte <monte6198@gmail.com>
2023-10-26 22:07:46 +0900

    feat: 카카오 로그인 추가 중

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
 class AuthController {
   kakaoLogin = (req, res) => {
-    //
+    // const { code } = req.body;
+    console.log('req: ', req.body);
+    // console.log(code);
+    res.status(201).json({
+      status: 'success',
+      message: '카카오 로그인 성공',
+    });
   };
 }
 
